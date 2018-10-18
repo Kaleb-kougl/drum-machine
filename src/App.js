b14ea0884a4f23c717d09b4dbff55d66502e2a3b
Kaleb Kougl <kalebkougl@gmail.com>
2018-10-17 19:25:00 -0500

    removed extra import statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Card } from './components/Card';
 import { DrumMachineView } from './components/DrumMachineView';
 
 class App extends Component {
